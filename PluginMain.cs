75c16650b7798119662c5ae41e780dd067c8d5e7
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-10-03 18:53:06 -0600

    Added scheduler task

--- PluginMain.cs
+++ PluginMain.cs
@@ -1,5 +1,10 @@
-﻿using ModuleShared;
+﻿using System;
+using ModuleShared;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using FileManagerPlugin;
 using MinecraftModule;
 
 //Your namespace must match the assembly name and the filename. Do not change one without changing the other two.
@@ -7,13 +12,14 @@ namespace MCAddonPlugin {
     //The first class must be called PluginName
     public class PluginMain : AMPPlugin {
         private readonly Settings _settings;
-        private readonly ILogger log;
+        private readonly ILogger _log;
         private readonly IConfigSerializer _config;
         private readonly IPlatformInfo platform;
         private readonly IRunningTasksManager _tasks;
-        private readonly MinecraftApp app;
         private readonly IPluginMessagePusher message;
-        private readonly IFeatureManager features;
+        private readonly IFeatureManager _features;
+        private readonly MinecraftApp _app;
+        private readonly WebMethods webMethods;
 
         //All constructor arguments after currentPlatform are optional, and you may ommit them if you don't
         //need that particular feature. The features you request don't have to be in any particular order.
@@ -24,15 +30,18 @@ namespace MCAddonPlugin {
             //These are the defaults, but other mechanisms are available.
             config.SaveMethod = PluginSaveMethod.KVP;
             config.KVPSeparator = "=";
-            this.log = log;
+            _log = log;
             _config = config;
             this.platform = platform;
             _settings = config.Load<Settings>(AutoSave: true); //Automatically saves settings when they're changed.
             _tasks = taskManager;
-            app = (MinecraftApp) Application;
             message = Message;
-            features = Features;
+            _features = Features;
             _settings.SettingModified += Settings_SettingModified;
+            
+            _app = (MinecraftApp) Application;
+            
+            webMethods = new WebMethods(this, _settings, log, Features, _app);
         }
 
         /*
@@ -46,7 +55,7 @@ namespace MCAddonPlugin {
         //Your init function should not invoke any code that depends on other plugins.
         //You may expose functionality via IFeatureManager.RegisterFeature, but you cannot yet use RequestFeature.
         public override void Init(out WebMethodsBase APIMethods) {
-            APIMethods = new WebMethods(log, features, _settings, app);
+            APIMethods = webMethods;
         }
 
         void Settings_SettingModified(object sender, SettingModifiedEventArgs e) {
@@ -60,5 +69,183 @@ namespace MCAddonPlugin {
         public override void PostInit() { }
 
         public override IEnumerable<SettingStore> SettingStores => Utilities.EnumerableFrom(_settings);
+        
+        public ActionResult SetServerInfo(MCConfig.ServerType serverType, MinecraftVersion minecraftVersion, bool deleteWorld = false) {
+            // BEFORE
+            _log.Debug("PreServerType: " + _app.Module.settings.Minecraft.ServerType);
+            _log.Debug("PreLevelName: " + _app.Module.settings.Minecraft.LevelName);
+            _log.Debug("PreJava: " + _app.Module.settings.Java.JavaVersion);
+            
+            var mcVersion = minecraftVersion.ToString().Substring(1).Replace("_", ".");
+            
+            object versionInfo = null;
+            switch (serverType) {
+                case MCConfig.ServerType.Forge:
+                    foreach (var version in _app.Module.updates.ForgeVersionInfo.number) {
+                        if (!mcVersion.Equals(version.Key.Split("-")[0])) continue;
+                        _log.Debug("PreVersion: " + _app.Module.settings.Minecraft.SpecificForgeVersion);
+                        versionInfo = version.Value.ToString();
+                        _app.Module.settings.Minecraft.ServerType = serverType;
+                        _app.Module.settings.Minecraft.SpecificForgeVersion = version.Value.ToString();
+                        break;
+                    }
+                    break;
+                case MCConfig.ServerType.NeoForge:
+                    foreach (var version in _app.Module.updates.NeoForgeVersionInfo.number) {
+                        if (!version.Key.StartsWith(mcVersion.Substring(2, mcVersion.Length - 2))) continue;
+                        _log.Debug("PreVersion: " + _app.Module.settings.Minecraft.SpecificNeoForgeVersion);
+                        versionInfo = version.Value.ToString();
+                        _app.Module.settings.Minecraft.ServerType = serverType;
+                        _app.Module.settings.Minecraft.SpecificNeoForgeVersion = version.Value.ToString();
+                        break;
+                    }
+                    break;
+                default:
+                    return ActionResult.FailureReason("Could not parse the server type, or the server type is not supported.");
+            }
+            if (versionInfo == null) {
+                return ActionResult.FailureReason("Could not find the version info for the specified server type and Minecraft version.");
+            }
+            
+            // Set the correct Java runtime version
+            UpdateJavaVersion(minecraftVersion);
+            
+            // Change the world name to the MC version
+            if (!_app.Module.settings.Minecraft.LevelName.EndsWith(mcVersion)) {
+                _app.Module.settings.Minecraft.LevelName = $"world_{mcVersion}";
+            }
+            
+            // Delete the world folder if requested
+            if (deleteWorld || _settings.MainSettings.DelWorldFolder) {
+                var fileManager = (IVirtualFileService) _features.RequestFeature<IWSTransferHandler>();
+                var worldFolder = fileManager.GetDirectory($"world_{mcVersion}");
+                if (worldFolder.Exists) {
+                    _log.Debug("Deleting world folder: " + worldFolder.FullName);
+                    worldFolder.Delete(true);
+                } else {
+                    _log.Debug("World folder does not exist: " + worldFolder.FullName);
+                }
+            }
+            
+            _app.Module.config.Save(_app.Module.settings);
+            
+            // AFTER
+            _log.Debug("PostServerType: " + _app.Module.settings.Minecraft.ServerType);
+            _log.Debug("PostLevelName: " + _app.Module.settings.Minecraft.LevelName);
+            _log.Debug("PostJava: " + _app.Module.settings.Java.JavaVersion);
+            _log.Debug("PostVersion: " + versionInfo);
+            
+            // Update the modloader to fix server starting
+            // TODO: Remove when fixed internally
+            if (ShouldUpdate(serverType, minecraftVersion)) {
+                _app.Update();
+            }
+            
+            return ActionResult.Success;
+        }
+
+        [SuppressMessage("ReSharper", "SwitchStatementMissingSomeEnumCasesNoDefault")]
+        private static bool ShouldUpdate(MCConfig.ServerType serverType, MinecraftVersion minecraftVersion) {
+            if (serverType != MCConfig.ServerType.Forge) {
+                return false;
+            }
+            
+            switch (minecraftVersion) {
+                case MinecraftVersion.V1_17:
+                case MinecraftVersion.V1_17_1:
+                case MinecraftVersion.V1_18:
+                case MinecraftVersion.V1_18_1:
+                case MinecraftVersion.V1_18_2:
+                case MinecraftVersion.V1_19:
+                case MinecraftVersion.V1_19_1:
+                case MinecraftVersion.V1_19_2:
+                case MinecraftVersion.V1_19_3:
+                case MinecraftVersion.V1_19_4:
+                case MinecraftVersion.V1_20:
+                case MinecraftVersion.V1_20_1:
+                case MinecraftVersion.V1_20_2:
+                    return false;
+            }
+            return true;
+        }
+
+        private void UpdateJavaVersion(MinecraftVersion minecraftVersion) {
+            // private Dictionary<string, string> _app.GetJavaVersions();
+            Dictionary<string, string> javaVersions;
+            try {
+                MethodInfo method = _app.GetType().GetMethod("GetJavaVersions", BindingFlags.NonPublic | BindingFlags.Instance);
+                // ReSharper disable once PossibleNullReferenceException
+                javaVersions = (Dictionary<string, string>) method.Invoke(_app, null);
+            } catch (Exception e) {
+                _log.Error("Error getting Java versions: " + e.Message);
+                return;
+            }
+            
+            switch (minecraftVersion) {
+                case MinecraftVersion.V1_14:
+                case MinecraftVersion.V1_14_1:
+                case MinecraftVersion.V1_14_2:
+                case MinecraftVersion.V1_14_3:
+                case MinecraftVersion.V1_14_4:
+                    // Java 8
+                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-8-")))
+                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
+                    break;
+                case MinecraftVersion.V1_15:
+                case MinecraftVersion.V1_15_1:
+                case MinecraftVersion.V1_15_2:
+                case MinecraftVersion.V1_16:
+                case MinecraftVersion.V1_16_1:
+                case MinecraftVersion.V1_16_2:
+                case MinecraftVersion.V1_16_3:
+                case MinecraftVersion.V1_16_4:
+                case MinecraftVersion.V1_16_5:
+                    // Java 11
+                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-11-")))
+                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
+                    break;
+                case MinecraftVersion.V1_17:
+                case MinecraftVersion.V1_17_1:
+                case MinecraftVersion.V1_18:
+                case MinecraftVersion.V1_18_1:
+                case MinecraftVersion.V1_18_2:
+                case MinecraftVersion.V1_19:
+                case MinecraftVersion.V1_19_1:
+                case MinecraftVersion.V1_19_2:
+                case MinecraftVersion.V1_19_3:
+                case MinecraftVersion.V1_19_4:
+                case MinecraftVersion.V1_20:
+                case MinecraftVersion.V1_20_1:
+                case MinecraftVersion.V1_20_2:
+                case MinecraftVersion.V1_20_3:
+                case MinecraftVersion.V1_20_4:
+                    // Java 17
+                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-17-")))
+                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
+                    break;
+                case MinecraftVersion.V1_20_5:
+                case MinecraftVersion.V1_20_6:
+                case MinecraftVersion.V1_21:
+                case MinecraftVersion.V1_21_1:
+                case MinecraftVersion.V1_21_2:
+                default:
+                    // Java 21
+                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-21-")))
+                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
+                    break;
+            }
+        }
+        
+        public enum NoYes {
+            No,
+            Yes
+        }
+        
+        [ScheduleableTask("Switch the server to a different modloader and/or version.")]
+        public ActionResult ScheduleSetServerInfo(
+            [ParameterDescription("The server type or modloader to use")] MCConfig.ServerType serverType,
+            [ParameterDescription("The version of Minecraft to use")] MinecraftVersion minecraftVersion,
+            [ParameterDescription("Delete the world folder when setting up the server")] NoYes deleteWorld = NoYes.No)
+            => SetServerInfo(serverType, minecraftVersion, deleteWorld == NoYes.Yes);
     }
 }
--- WebMethods.cs
+++ WebMethods.cs
@@ -9,12 +9,14 @@ using MinecraftModule;
 
 namespace MCAddonPlugin {
     internal class WebMethods : WebMethodsBase {
+        private readonly PluginMain _plugin;
         private readonly Settings _settings;
         private readonly IFeatureManager _features;
         private readonly ILogger _log;
         private readonly MinecraftApp _app;
 
-        public WebMethods(ILogger log, IFeatureManager features, Settings settings, MinecraftApp app) {
+        public WebMethods(PluginMain plugin, Settings settings, ILogger log, IFeatureManager features, MinecraftApp app) {
+            _plugin = plugin;
             _settings = settings;
             _features = features;
             _log = log;
@@ -26,15 +28,10 @@ namespace MCAddonPlugin {
         }
 
         [JSONMethod(
-            "",
-            "")]
+            "Switch the server to a different modloader or different version of Minecraft.",
+            "An ActionResult indicating the success or failure of the operation.")]
         [RequiresPermissions(MCAddonPluginPermissions.SetVersion)]
         public ActionResult SetServerInfo(string serverType = "", string mcVersion = "", bool deleteWorld = false) {
-            // BEFORE
-            _log.Debug("PreServerType: " + _app.Module.settings.Minecraft.ServerType);
-            _log.Debug("PreLevelName: " + _app.Module.settings.Minecraft.LevelName);
-            _log.Debug("PreJava: " + _app.Module.settings.Java.JavaVersion);
-            
             // Parse the platform and use the ServerType enum
             MCConfig.ServerType parsedType;
             if (string.IsNullOrEmpty(serverType)) {
@@ -47,162 +44,11 @@ namespace MCAddonPlugin {
             MinecraftVersion minecraftVersion;
             if (string.IsNullOrEmpty(mcVersion)) {
                 minecraftVersion = _settings.MainSettings.MinecraftVersion;
-                mcVersion = minecraftVersion.ToString().Substring(1).Replace("_", ".");
             } else {
                 Enum.TryParse("V" + mcVersion.Replace(".", "_"), out minecraftVersion);
             }
             
-            object versionInfo = null;
-            switch (parsedType) {
-                case MCConfig.ServerType.Forge:
-                    foreach (var version in _app.Module.updates.ForgeVersionInfo.number) {
-                        if (!mcVersion.Equals(version.Key.Split("-")[0])) continue;
-                        _log.Debug("PreVersion: " + _app.Module.settings.Minecraft.SpecificForgeVersion);
-                        versionInfo = version.Value.ToString();
-                        _app.Module.settings.Minecraft.ServerType = parsedType;
-                        _app.Module.settings.Minecraft.SpecificForgeVersion = version.Value.ToString();
-                        break;
-                    }
-                    break;
-                case MCConfig.ServerType.NeoForge:
-                    foreach (var version in _app.Module.updates.NeoForgeVersionInfo.number) {
-                        if (!version.Key.StartsWith(mcVersion.Substring(2, mcVersion.Length - 2))) continue;
-                        _log.Debug("PreVersion: " + _app.Module.settings.Minecraft.SpecificNeoForgeVersion);
-                        versionInfo = version.Value.ToString();
-                        _app.Module.settings.Minecraft.ServerType = parsedType;
-                        _app.Module.settings.Minecraft.SpecificNeoForgeVersion = version.Value.ToString();
-                        break;
-                    }
-                    break;
-            }
-            
-            // Set the correct Java runtime version
-            UpdateJavaVersion(minecraftVersion);
-            
-            // Change the world name to the MC version
-            if (!_app.Module.settings.Minecraft.LevelName.EndsWith(mcVersion)) {
-                _app.Module.settings.Minecraft.LevelName = $"world_{mcVersion}";
-            }
-            
-            // Delete the world folder if requested
-            if (deleteWorld || _settings.MainSettings.DelWorldFolder) {
-                var fileManager = (IVirtualFileService) _features.RequestFeature<IWSTransferHandler>();
-                var worldFolder = fileManager.GetDirectory($"world_{mcVersion}");
-                if (worldFolder.Exists) {
-                    _log.Debug("Deleting world folder: " + worldFolder.FullName);
-                    worldFolder.Delete(true);
-                } else {
-                    _log.Debug("World folder does not exist: " + worldFolder.FullName);
-                }
-            }
-            
-            _app.Module.config.Save(_app.Module.settings);
-            
-            // AFTER
-            _log.Debug("PostServerType: " + _app.Module.settings.Minecraft.ServerType);
-            _log.Debug("PostLevelName: " + _app.Module.settings.Minecraft.LevelName);
-            _log.Debug("PostJava: " + _app.Module.settings.Java.JavaVersion);
-            _log.Debug("PostVersion: " + versionInfo);
-            
-            // Update the modloader to fix server starting
-            // TODO: Remove when fixed internally
-            if (ShouldUpdate(parsedType, minecraftVersion)) {
-                _app.Update();
-            }
-            
-            return ActionResult.Success;
-        }
-
-        [SuppressMessage("ReSharper", "SwitchStatementMissingSomeEnumCasesNoDefault")]
-        private static bool ShouldUpdate(MCConfig.ServerType serverType, MinecraftVersion minecraftVersion) {
-            if (serverType != MCConfig.ServerType.Forge) {
-                return false;
-            }
-            
-            switch (minecraftVersion) {
-                case MinecraftVersion.V1_17:
-                case MinecraftVersion.V1_17_1:
-                case MinecraftVersion.V1_18:
-                case MinecraftVersion.V1_18_1:
-                case MinecraftVersion.V1_18_2:
-                case MinecraftVersion.V1_19:
-                case MinecraftVersion.V1_19_1:
-                case MinecraftVersion.V1_19_2:
-                case MinecraftVersion.V1_19_3:
-                case MinecraftVersion.V1_19_4:
-                case MinecraftVersion.V1_20:
-                case MinecraftVersion.V1_20_1:
-                case MinecraftVersion.V1_20_2:
-                    return false;
-            }
-            return true;
-        }
-
-        private void UpdateJavaVersion(MinecraftVersion minecraftVersion) {
-            // private Dictionary<string, string> _app.GetJavaVersions();
-            Dictionary<string, string> javaVersions;
-            try {
-                MethodInfo method = _app.GetType().GetMethod("GetJavaVersions", BindingFlags.NonPublic | BindingFlags.Instance);
-                // ReSharper disable once PossibleNullReferenceException
-                javaVersions = (Dictionary<string, string>) method.Invoke(_app, null);
-            } catch (Exception e) {
-                _log.Error("Error getting Java versions: " + e.Message);
-                return;
-            }
-            
-            switch (minecraftVersion) {
-                case MinecraftVersion.V1_14:
-                case MinecraftVersion.V1_14_1:
-                case MinecraftVersion.V1_14_2:
-                case MinecraftVersion.V1_14_3:
-                case MinecraftVersion.V1_14_4:
-                    // Java 8
-                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-8-")))
-                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
-                    break;
-                case MinecraftVersion.V1_15:
-                case MinecraftVersion.V1_15_1:
-                case MinecraftVersion.V1_15_2:
-                case MinecraftVersion.V1_16:
-                case MinecraftVersion.V1_16_1:
-                case MinecraftVersion.V1_16_2:
-                case MinecraftVersion.V1_16_3:
-                case MinecraftVersion.V1_16_4:
-                case MinecraftVersion.V1_16_5:
-                    // Java 11
-                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-11-")))
-                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
-                    break;
-                case MinecraftVersion.V1_17:
-                case MinecraftVersion.V1_17_1:
-                case MinecraftVersion.V1_18:
-                case MinecraftVersion.V1_18_1:
-                case MinecraftVersion.V1_18_2:
-                case MinecraftVersion.V1_19:
-                case MinecraftVersion.V1_19_1:
-                case MinecraftVersion.V1_19_2:
-                case MinecraftVersion.V1_19_3:
-                case MinecraftVersion.V1_19_4:
-                case MinecraftVersion.V1_20:
-                case MinecraftVersion.V1_20_1:
-                case MinecraftVersion.V1_20_2:
-                case MinecraftVersion.V1_20_3:
-                case MinecraftVersion.V1_20_4:
-                    // Java 17
-                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-17-")))
-                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
-                    break;
-                case MinecraftVersion.V1_20_5:
-                case MinecraftVersion.V1_20_6:
-                case MinecraftVersion.V1_21:
-                case MinecraftVersion.V1_21_1:
-                case MinecraftVersion.V1_21_2:
-                default:
-                    // Java 21
-                    foreach (var keyValuePair in javaVersions.Where(vkp => vkp.Key.Contains("-21-")))
-                    { _app.Module.settings.Java.JavaVersion = keyValuePair.Key; }
-                    break;
-            }
+            return _plugin.SetServerInfo(parsedType, minecraftVersion, deleteWorld);
         }
     }
 }
